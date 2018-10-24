33b97fa88a78ccd55e3a40739e6c2eabcc055d71
unknown <xande1231231@hotmail.com>
2018-10-24 18:03:13 -0300

    Fix

--- javascript/navigation-header-bar.js
+++ javascript/navigation-header-bar.js
@@ -63,7 +63,7 @@ function closeleftbar(){
 	if(navigation_leftbar == "1"){
 	navigation_leftbar = "1";
 	on_leftbar = "1";
-	idbutton = "1	";
+	idbutton = "1";
 		playAcess();
 		$("#guide-101").css("background", "transparent");
 		$("#guide-102").css("background", "#fff");
